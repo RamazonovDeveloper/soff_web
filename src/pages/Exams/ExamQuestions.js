6dcaccf7673f10a160e2f46e622fbe232576c0c6
RamazonovDeveloper <ramazonovferuz01@gmail.com>
2023-04-02 12:31:41 +0500

    Changed test page

--- src/pages/Exams/ExamQuestions.js
+++ src/pages/Exams/ExamQuestions.js
@@ -32,12 +32,29 @@ function ExamQuestions() {
       <div className='questions_tests'>
         {
           questions.map((item, index)=>{
-            return <div className='questions_tests_item'>
+            return <div key={index} className='questions_tests_item'>
               <div className='questions_tests_item_text'>
                 <p>{index + 1}.</p>
-                <img key={index} src={item.image}/>
+                <img className='questions_tests_item_text_img' key={index} src={item.image}/>
               </div>
-              
+              <div className='questions_tests_item_answers'>
+                <div className='questions_tests_item_answers_answer'>
+                  <input type="radio" id="a" name="question_answers" value="A"/>
+                  <label for="a">A</label>
+                </div>
+                <div className='questions_tests_item_answers_answer'>
+                  <input type="radio" id="b" name="question_answers" value="B"/>
+                  <label for="b">B</label>
+                </div>
+                <div className='questions_tests_item_answers_answer'>
+                  <input type="radio" id="c" name="question_answers" value="C"/>
+                  <label for="c">C</label>
+                </div>
+                <div className='questions_tests_item_answers_answer'>
+                  <input type="radio" id="d" name="question_answers" value="D"/>
+                  <label for="d">D</label>
+                </div>
+              </div>
             </div>
           })
         }
